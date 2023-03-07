1292b3b7af3e35c4e5b85bbfa8590935041ab91e
Aleksey Novikov <aksnvkv@mail.ru>
2023-03-08 00:19:27 +0300

    add modal, portal, change header

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,37 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true
+    jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb', 'plugin:i18next/recommended',
+    'plugin:storybook/recommended',
+  ],
   overrides: [{
-    files: ['**/src/**/*.test.{ts,tsx}'],
+    files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
     rules: {
-      'i18next/no-literal-string': 'off'
-    }
+      'i18next/no-literal-string': 'off',
+      'max-len': 'off',
+    },
   }],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'i18next'],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'i18next',
+    'react-hooks',
+  ],
   rules: {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
     indent: [2, 2],
     'react/jsx-filename-extension': [2, {
-      extensions: ['.js', '.jsx', 'tsx']
+      extensions: ['.js', '.jsx', 'tsx'],
     }],
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
@@ -40,14 +50,18 @@ module.exports = {
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', {
       markupOnly: true,
-      ignoreAttribute: ['data-testid', 'to']
+      ignoreAttribute: ['data-testid', 'to'],
     }],
     'max-len': ['error', {
       ignoreComments: true,
-      code: 100
-    }]
+      code: 100,
+    }],
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
   },
   globals: {
-    __IS_DEV__: true
-  }
+    __IS_DEV__: true,
+  },
 };
