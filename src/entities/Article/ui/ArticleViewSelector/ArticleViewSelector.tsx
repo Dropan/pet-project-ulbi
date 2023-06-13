4b6317274b746dc6e721f3066c2d4eebe37ce60d
Aleksey Novikov <aksnvkv@mail.ru>
2023-06-13 16:59:48 +0300

    add fetching articles, view type selector

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,59 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Text } from 'shared/ui/Text/Text';
+import { Icon } from 'shared/ui/Icon/Icon';
+import EyeIcon from 'shared/assets/icons/eye.svg';
+import { Card } from 'shared/ui/Card/Card';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import ListIcon from 'shared/assets/icons/bi_list.svg';
+import TiledIcon from 'shared/assets/icons/fe_tiled.svg';
+import cls from './ArticleViewSelector.module.scss';
+import { ArticleView } from '../../model/types/article';
+
+interface ArticleViewSelectorProps {
+  className?: string;
+  view: ArticleView;
+  onViewClick?: (view: ArticleView) => void
+}
+
+const viewType = [
+  {
+    view: ArticleView.SMALL,
+    icon: TiledIcon,
+  },
+  {
+    view: ArticleView.BIG,
+    icon: ListIcon,
+  },
+];
+
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+  const {
+    className, view, onViewClick,
+  } = props;
+  const { t } = useTranslation('article-details');
+
+  const onClick = (newView: ArticleView) => () => () => {
+    onViewClick?.(newView);
+  };
+
+  return (
+    <div className={classNames(cls.ArticleViewSelector, {}, [className, cls[view]])}>
+      {viewType.map((viewType) => (
+        <Button
+          theme={ButtonTheme.CLEAR}
+          onClick={onClick(viewType.view)}
+        >
+          <Icon
+            Svg={viewType.icon}
+            className={classNames('', { [cls.notSelected]: viewType.view !== view })}
+          />
+        </Button>
+      ))}
+    </div>
+  );
+});
