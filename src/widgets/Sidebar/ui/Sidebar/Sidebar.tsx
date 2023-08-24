f978ba4a8e5f4a2feaf3cd155acfe3bdb1887357
Aleksey Novikov <aksnvkv@mail.ru>
2023-08-24 13:11:08 +0300

    semantics

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -19,7 +19,7 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
   const onToggle = () => setCollapsed((prev) => !prev);
 
   return (
-    <menu
+    <aside
       data-testid="sidebar"
       className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
     >
@@ -34,7 +34,7 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
       >
         {collapsed ? '>' : '<'}
       </Button>
-      <VStack gap="8" className={cls.items}>
+      <VStack role="navigation" gap="8" className={cls.items}>
         {sidebarItemsList.map((item) => (
           <SidebarItem
             key={item.path}
@@ -47,6 +47,6 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
         <ThemeSwitcher />
         <LangSwitcher short={collapsed} className={cls.lang} />
       </div>
-    </menu>
+    </aside>
   );
 });
