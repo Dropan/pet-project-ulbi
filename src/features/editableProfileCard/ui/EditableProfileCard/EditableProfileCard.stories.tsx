a6ccb943322fd2e35e04640f2bb6fa463c0f11fe
Aleksey Novikov <aksnvkv@mail.ru>
2023-08-29 11:03:17 +0300

    fix

--- src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.stories.tsx
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.stories.tsx
@@ -4,16 +4,16 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { EditableProfileCard } from './EditableProfileCard';
 
 export default {
-    title: 'features/EditableProfileCard',
-    component: EditableProfileCard,
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
+  title: 'features/EditableProfileCard',
+  component: EditableProfileCard,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 } as ComponentMeta<typeof EditableProfileCard>;
 
 const Template: ComponentStory<typeof EditableProfileCard> = (args) => <EditableProfileCard {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {
-   
+
 };
--- src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 import { memo, useCallback } from 'react';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { Currency } from 'entities/Currency';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
@@ -11,7 +10,6 @@ import { Country } from 'entities/Country';
 import { ProfileCard } from 'entities/Profile';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { VStack } from 'shared/ui/Stack';
-import cls from './EditableProfileCard.module.scss';
 import { getProfileForm } from '../../model/selectors/getProfileForm/getProfileForm';
 import { getProfileIsLoading } from '../../model/selectors/getProfileIsLoading/getProfileIsLoading';
 import { getProfileError } from '../../model/selectors/getProfileError/getProfileError';
@@ -93,7 +91,7 @@ export const EditableProfileCard = memo((props: EditableProfileCardProps) => {
       <VStack
         gap="8"
         max
-        className={classNames(cls.EditableProfileCard, {}, [className])}
+        className={classNames('', {}, [className])}
       >
         <EditableProfileCardHeader />
         {validateErrors?.length && validateErrors.map((err) => (
