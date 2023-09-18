09d7b5b5160b93c7ef84b5491365dcec25574e72
Aleksey Novikov <aksnvkv@mail.ru>
2023-09-18 23:35:25 +0300

    update custom eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -75,7 +75,7 @@ module.exports = {
     'no-param-reassign': 'off',
     'no-undef': 'off',
     'react/no-array-index-key': 'off',
-    'ulbi-tv-plugin-test/path-checker': 'error',
+    'ulbi-tv-plugin-test/path-checker': ['error', { alias: '@' }],
   },
   globals: {
     __IS_DEV__: true,
