22e42ebd6047213ae277cf0d87b152de8fd89ab9
Aleksey Novikov <aksnvkv@mail.ru>
2023-11-18 17:25:08 +0300

    add e2e tests with cypress

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -38,6 +38,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
   return (
     <DynamicModuleLoader reducers={reducers}>
       <Page
+        data-testid="ArticlesPage"
         onScrollEnd={onLoadNextPart}
         className={classNames(cls.ArticlesPage, {}, [className])}
       >
