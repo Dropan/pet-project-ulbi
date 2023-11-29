70d55680d583e1ce134194b956f86e12e75f2700
Aleksey Novikov <aksnvkv@mail.ru>
2023-11-29 13:55:49 +0300

    add feature flags concept

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -14,6 +14,7 @@ import cls from './ArticleDetailsPage.module.scss';
 import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
 import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
 import { ArticleRating } from '@/features/articleRating';
+import { getFeatureFlag } from '@/shared/lib/features';
 
 interface ArticleDetailsPageProps {
   className?: string;
@@ -27,6 +28,7 @@ export const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
   const { className } = props;
   // const { t } = useTranslation('article-details');
   const { id } = useParams<{ id: string }>();
+  const isArticleRatingEnabled = getFeatureFlag('isArticleRatingEnabled');
 
   if (!id) {
     return null;
@@ -38,7 +40,7 @@ export const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
         <VStack gap="16" max>
           <ArticleDetailsPageHeader />
           <ArticleDetails id={id} />
-          <ArticleRating articleId={id} />
+          {isArticleRatingEnabled && <ArticleRating articleId={id} />}
           <ArticleRecommendationsList />
           <ArticleDetailsComments id={id} />
         </VStack>
---
+++ src/shared/lib/features/setGetFeatures.ts
@@ -0,0 +1,14 @@
+import { FeatureFlags } from "@/shared/types/featureFlags";
+
+// Фичи в ходе сессии не меняются, их необязательно делать реактивными
+let featureFlags: FeatureFlags;
+
+export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
+  if (newFeatureFlags) {
+    featureFlags = newFeatureFlags;
+  }
+}
+
+export function getFeatureFlag(flag: keyof FeatureFlags) {
+  return featureFlags[flag];
+}
