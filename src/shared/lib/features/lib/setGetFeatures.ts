dc3fd4947c8aa40d6c9dede38f1b9a2a0de88181
Aleksey Novikov <aksnvkv@mail.ru>
2024-02-26 18:39:18 +0300

    add loading layout, fallback theme, default features

--- src/shared/lib/features/lib/setGetFeatures.ts
+++ src/shared/lib/features/lib/setGetFeatures.ts
@@ -1,7 +1,15 @@
+import { LOCAL_STORAGE_LAST_DESIGN_KEY } from '@/shared/const/localStorage';
 import { FeatureFlags } from '@/shared/types/featureFlags';
 
+const defaultFeatures: FeatureFlags = {
+  isAppRedesigned:
+    localStorage.getItem(LOCAL_STORAGE_LAST_DESIGN_KEY) === 'new',
+};
+
 // Фичи в ходе сессии не меняются, их необязательно делать реактивными
-let featureFlags: FeatureFlags = {};
+let featureFlags: FeatureFlags = {
+  ...defaultFeatures,
+};
 
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
   if (newFeatureFlags) {
